4b95acc0570374d8974ef388f828c2e28265bbf7
Mragansh Tomar <mragansh.tomar@synapse.com>
2018-07-20 13:44:14 +0530

    Initated !

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/fun', function (req, res) {
+    let func = `function(){\n
+        return "Hello World"\n
+    }\n`;
+
+    func = func + "\n Hello World!"
+    res.send(func);
+  });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
